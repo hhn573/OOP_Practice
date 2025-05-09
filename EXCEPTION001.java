506efd70b128ae965ba59bd7ec95ec20bb6dfd84
hhn573 <NghiaHH.B21VT322@stu.ptit.edu.vn>
2025-05-09 13:48:36 +0700

    Updaate

--- EXCEPTION001.java
+++ EXCEPTION001.java
@@ -1,53 +1,62 @@
-    import java.util.*;
+import java.util.*;
 
-    class Amount {
-        private String currency;
-        private int amount;
+class Amount {
+    private String currency;
+    private int amount;
 
-        public Amount(String currency, int amount) {
-            this.currency = currency;
-            this.amount = amount;
+    public Amount(String currency, int amount) {
+        this.currency = currency;
+        this.amount = amount;
+    }
+
+    public void add(Amount amount) {
+        if (this.currency.equals(amount.currency)) {
+            this.amount += amount.amount;
+        } else {
+            System.out.println("Currency doesn't match");
         }
+    }
 
-        public void add(Amount amount) {
-        if(this.currency.equals(amount.currency)) {
-                this.amount += amount.amount;
-            } 
-            else {
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Amount)) return false;
+        Amount other = (Amount) obj;
+        return this.currency.equals(other.currency);
+    }
+
+    @Override
+    public String toString() {
+        return amount + " " + currency;
+    }
+}
+
+public class EXCEPTION001 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int t = scanner.nextInt();
+        scanner.nextLine(); // consume newline
+
+        for (int i = 0; i < t; i++) {
+            String currency1 = scanner.nextLine();
+            int amount1 = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            String currency2 = scanner.nextLine();
+            int amount2 = scanner.nextInt();
+            scanner.nextLine(); // consume newline
+
+            Amount am1 = new Amount(currency1, amount1);
+            Amount am2 = new Amount(currency2, amount2);
+
+            if (am1.equals(am2)) {
+                am1.add(am2);
+                System.out.println(am1);
+            } else {
                 System.out.println("Currency doesn't match");
             }
         }
+
+        scanner.close();
     }
-
-    public class EXCEPTION001 {
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            int t = scanner.nextInt();
-            scanner.nextLine(); // Consume the newline character
-
-            for (int i = 0; i < t; i++) {
-                
-                String currency1 = scanner.nextLine();
-                int amount1 = scanner.nextInt();
-                scanner.nextLine(); 
-
-                String currency2 = scanner.nextLine();
-                int amount2 = scanner.nextInt();
-                scanner.nextLine(); 
-            
-                Amount am1 = new Amount(currency1, amount1);
-                Amount am2 = new Amount(currency2, amount2);
-
-                if (am1.equals(am2)) {
-                    amt1.add(amt2);
-                    System.out.println(am1);
-                } 
-                else {
-                    System.out.println("Currency doesn't match");
-                    
-                }
-
-            }
-            scanner.close();
-        }
-    }
+}
